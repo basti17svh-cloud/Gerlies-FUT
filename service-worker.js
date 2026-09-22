ecb5f1119157325cc30a59cfe5f1ff13828745b1
basti17svh-cloud <basti17svh@gmail.com>
2026-09-22 14:47:07 +0200

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE="gerlies-fut-v12-shell";
+const CACHE="gerlies-fut-v16-shell";
 const SHELL=["./","./index.html","./manifest.webmanifest","./players-fallback.json","./icon-192.png","./icon-512.png","./icon-maskable-512.png","./apple-touch-icon.png","./assets/portraits/p1.png","./assets/portraits/p2.png","./assets/portraits/p3.png","./assets/portraits/p4.png","./assets/portraits/p5.png","./assets/portraits/p6.png","./assets/portraits/p7.png","./assets/portraits/p8.png"];
 self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(SHELL)).then(()=>self.skipWaiting())));
 self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
